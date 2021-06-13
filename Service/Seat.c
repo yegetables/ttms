1106776af112baaf00c8a53b588008ad9d1ab847
Raja <1647193241@qq.com>
2021-06-13 16:59:23 +0800

    Add some note

--- Service/Seat.c
+++ Service/Seat.c
@@ -13,19 +13,21 @@
 #include <stdlib.h>
 
 #include "../Common/List.h"
+#include "../Persistence/Seat_Persist.h"
 
-/*MARK:函数功能：用于添加一个新座位数据。
+/*TODO:增加座位:添加新座位
+MARK:函数功能：用于添加一个新座位数据。
 参数说明：data为seat_t类型指针，表示需要添加的座位数据结点。
 返 回 值：整型，表示是否成功添加了座位的标志。
 */
 int Seat_Srv_Add(const seat_t *data)
 {
     // 请补充完整
-    
+    Seat_Perst_Insert(data);  //存储新座位
     return 0;
 }
 
-/*
+/*TODO:批量增加座位数据
 函数功能：批量添加座位数据。
 参数说明：list为seat_list_t类型指针，表示需要添加的批量座位数据形成的链表。
 返 回 值：整型，表示是否成功添加了批量座位的标志。
@@ -36,7 +38,7 @@ int Seat_Srv_AddBatch(seat_list_t list)
     return 0;
 }
 
-/*
+/*TODO:修改座位:更新座位属性
 函数功能：用于修改一个座位数据。
 参数说明：data为seat_t类型指针，表示需要修改的座位数据结点。
 返 回 值：整型，表示是否成功修改了座位的标志。
@@ -44,10 +46,11 @@ int Seat_Srv_AddBatch(seat_list_t list)
 int Seat_Srv_Modify(const seat_t *data)
 {
     // 请补充完整
+    Seat_Perst_Update(data);  //在文件中更新座位数据
     return 0;
 }
 
-/*
+/*TODO:删除座位:根据座位ID删除座位
 函数功能：根据座位ID删除一个座位。
 参数说明：ID为整型，表示需要删除的座位数据结点。
 返 回 值：整型，表示是否成功删除了座位的标志。
@@ -55,10 +58,11 @@ int Seat_Srv_Modify(const seat_t *data)
 int Seat_Srv_DeleteByID(int ID)
 {
     // 请补充完整
+    Seat_Srv_DeleteByID(ID);  //删除座位
     return 1;
 }
 
-/*
+/*TODO:管理座位:通过ID获取所有座位
 函数功能：根据座位ID获取座位数据。
 参数说明：第一个参数ID为整型，表示座位ID，第二个参数buf为seat_t指针，指向待获取的座位数据结点。
 返 回 值：整型，表示是否成功获取了座位的标志。
@@ -69,7 +73,7 @@ int Seat_Srv_FetchByID(int ID, seat_t *buf)
     return 0;
 }
 
-/*
+/*TODO:删除座位:根据影厅ID删除所有座位
 函数功能：根据演出厅ID删除所有座位。
 参数说明：roomID为整型，表示需要删除所有座位的演出厅ID。
 返 回 值：整型，表示是否成功删除了演出厅所有座位的标志。
@@ -80,7 +84,7 @@ inline int Seat_Srv_DeleteAllByRoomID(int roomID)
     return 0;
 }
 
-/*
+/*TODO:根据影厅ID初始化所有座位
 函数功能：根据给定演出厅的行、列数初始化演出厅的所有座位数据，并将每个座位结点按行插入座位链表。
 参数说明：第一个参数list为seat_list_t类型指针，指向座位链表头指针，第二个参数rowsCount为整型，表示座位所在行号，第三个参数colsCount为整型，表示座位所在列号。
 返 回 值：整型，表示是否成功初始化了演出厅的所有座位。
@@ -91,7 +95,7 @@ int Seat_Srv_FetchByRoomID(seat_list_t list, int roomID)
     return 0;
 }
 
-/*
+/*TODO:根据影厅ID获取有效座位
 函数功能：根据演出厅ID获得该演出厅的有效座位。
 参数说明：第一个参数list为seat_list_t类型，表示获取到的有效座位链表头指针，第二个参数roomID为整型，表示需要提取有效座位的演出厅ID。
 返 回 值：整型，表示演出厅的有效座位个数。
@@ -102,7 +106,7 @@ int Seat_Srv_FetchValidByRoomID(seat_list_t list, int roomID)
     return 0;
 }
 
-/*
+/*TODO:管理座位:初始化演出厅座位
 函数功能：根据给定演出厅的行、列数初始化演出厅的所有座位数据，并将每个座位结点按行插入座位链表。
 参数说明：第一个参数list为seat_list_t类型指针，指向座位链表头指针，第二个参数rowsCount为整型，表示座位所在行号，第三个参数colsCount为整型，表示座位所在列号。
 返 回 值：整型，表示是否成功初始化了演出厅的所有座位。
@@ -111,10 +115,11 @@ int Seat_Srv_RoomInit(seat_list_t list, int roomID, int rowsCount,
                       int colsCount)
 {
     // 请补充完整
+    Seat_Perst_InsertBatch(list);  //批量存储座位
     return 0;
 }
 
-/*
+/*TODO:管理座位:对链表排序
 函数功能：对座位链表list按座位行号、列号进行排序。
 参数说明：list为seat_list_t类型，表示待排序座位链表头指针。
 返 回 值：无。
@@ -124,7 +129,7 @@ void Seat_Srv_SortSeatList(seat_list_t list)
     // 请补充完整
 }
 
-/*
+/*TODO:管理座位:增加结点到排序链表
 函数功能：将一个座位结点加入到已排序的座位链表中。
 参数说明：第一个参数list为seat_list_t类型，表示待插入结点的座位链表头指针，第二个参数node为seat_node_t指针，表示需要插入的座位数据结点。
 返 回 值：无。
@@ -134,7 +139,7 @@ void Seat_Srv_AddToSoftedList(seat_list_t list, seat_node_t *node)
     // 请补充完整
 }
 
-/*
+/*TODO:增加座位|修改座位|删除座位:判断是否存在
 函数功能：根据座位的行、列号获取座位数据。
 参数说明：第一个参数list为seat_list_t类型，表示座位链表头指针，
          第二个参数row为整型，表示待获取座位的行号，第三个参数column为整型，表示待获取座位的列号。
@@ -146,7 +151,7 @@ seat_node_t *Seat_Srv_FindByRowCol(seat_list_t list, int row, int column)
     return NULL;
 }
 
-/*
+/*TODO:根据座位ID获取座位数据
 函数功能：根据座位ID在链表中获取座位数据。
 参数说明：第一个参数list为seat_list_t类型，指向座位数据链表，第二个参数ID为整型，表示座位ID。
 返 回 值：seat_node_t类型，表示获取的座位数据。
