4d2c7bdaf76ecc7a3956e8c43ec606eb9fac2945
Raja <1647193241@qq.com>
2021-06-15 20:49:45 +0800

    qqq

--- Service/Salesanalysis.h
+++ Service/Salesanalysis.h
@@ -12,28 +12,29 @@
 
 #include "../Common/List.h"
 /**
- * @brief 
+ * @brief
  *  标识符：TTMS_SCU_SalesAnalysis_Srv_StaticSale
     功能：函数实现了构建salesanalysis_list_t类型链表功能
  */
-typedef struct 
+typedef struct
 {
-    int play_id;    //剧目编号
-    char name[31];  //剧目名称
-    char area[9];    //剧目区域
-    int duration;   //剧目播放时长
-    long totaltickets;  //剧目上座数量
-    long sales; //剧目票房（销售额统计）
-    int price;  //剧目票价
+    int play_id;            //剧目编号
+    char name[31];          //剧目名称
+    char area[9];           //剧目区域
+    int duration;           //剧目播放时长
+    long totaltickets;      //剧目上座数量
+    long sales;             //剧目票房（销售额统计）
+    int price;              //剧目票价
     ttms_date_tstart_date;  //剧目上映日期
     ttms_date_tend_date;    //剧目下映日期
 } salesanalysis_list_t;
 
-typedef struct salesanalysis_node {
-    salesanalysis_t data;   //实体数据
-    struct salesanalysis_node *next;     //后向指针
-    struct salesanalysis_node *prev;     //前向指针
-}salesanalysis_node_t, *salesanalysis_list_t;
+typedef struct salesanalysis_node
+{
+    salesanalysis_t data;             //实体数据
+    struct salesanalysis_node *next;  //后向指针
+    struct salesanalysis_node *prev;  //前向指针
+} salesanalysis_node_t, *salesanalysis_list_t;
 
 int SalesAnalysis_Srv_StaticSale(salesanalysis_list_t list);
 #endif /* salesanalysis_t */
