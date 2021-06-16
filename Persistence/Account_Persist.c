3ad31dd9c5753725856b91187e91a85f5df17217
PENG <1337903705@qq.com>
2021-06-16 18:05:30 +0800

    存修改个人信息界面

--- Persistence/Account_Persist.c
+++ Persistence/Account_Persist.c
@@ -1,5 +1,6 @@
 #include "Account_Persist.h"
 #include <unistd.h>
+#include<stdio.h>
 int Account_Perst_CheckAccFile()
 {
     int ret;
@@ -12,6 +13,33 @@ int Account_Perst_Insert(account_t* data)
 }
 int Account_Perst_Update(account_t* data)
 {
+    account_t tmp;
+    FILE*fp=fopen("Account.dat","rb+");
+    if (!fp)
+    {
+        printf("Account.dat fopen failed\n");
+        return 0;
+    }
+    else{
+        while(1)
+        {
+            if (fread(&tmp, sizeof(account_t), 1, fp)<1)
+            {
+                if (!feof(fp))//文件结束：返回非0值；文件未结束：返回0值
+                {
+                    printf("fread error\n");
+                    return 0;
+                }
+                break;
+            }
+            if (tmp.id==data->id)
+            {
+                fseek(fp,-(int)sizeof(account_t),SEEK_CUR);
+                fwrite(data,sizeof(account_t), 1, fp);
+            }
+        }
+    }
+    fclose(fp);
     return 1;
 }
 int Account_Perst_RemByID(int id)
--- Service/Account.c
+++ Service/Account.c
@@ -1,6 +1,38 @@
 #include "Account.h"
+#include<stdio.h>
 account_t Account_Srv_FetchbyUser(char usrName[])
 {
     account_t tmp;
     return tmp;
+}
+int Account_Srv_Modify(account_t *data)
+{
+    account_t tmp;
+    FILE*fp=fopen("Account.dat","rb+");
+    if (!fp)
+    {
+        printf("Account.dat fopen failed\n");
+        return 0;
+    }
+    else{
+        while(1)
+        {
+            if (fread(&tmp, sizeof(account_t), 1, fp)<1)
+            {
+                if (!feof(fp))//文件结束：返回非0值；文件未结束：返回0值
+                {
+                    printf("fread error\n");
+                    return 0;
+                }
+                break;
+            }
+            if (tmp.id==data->id)
+            {
+                fseek(fp,-(int)sizeof(account_t),SEEK_CUR);
+                fwrite(data,sizeof(account_t), 1, fp);
+            }
+        }
+    }
+    fclose(fp);
+    return 1;
 }
