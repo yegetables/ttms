93d5ac3558d999d8d2b481bd7821dfb7c48862bc
PENG <1337903705@qq.com>
2021-06-17 10:42:50 +0800

    asd

--- Persistence/Account_Persist.c
+++ Persistence/Account_Persist.c
@@ -1,6 +1,8 @@
 #include "Account_Persist.h"
 #include <unistd.h>
 #include<stdio.h>
+#include "../Common/List.h"
+#include"../Service/Account.h"
 int Account_Perst_CheckAccFile()
 {
     if (access("Account.dat",F_OK)==0)
@@ -66,6 +68,39 @@ int Account_Perst_RemByID(int id)
 int Account_Perst_SelectAll(account_list_t list)
 {
     int n;
+    List_Init(list,account_node_t)
+    account_node_t *p,*q;
+    account_t tmp;
+    FILE*fp=fopen("Account.dat","rb+");
+    if (!fp)
+    {
+        printf("Account.dat fopen failed\n");
+        return 0;
+    }
+    else{
+        while(1)
+        {
+            p=(account_node_t *)malloc(sizeof(account_node_t));
+            if (fread(&tmp, sizeof(account_t), 1, fp)<1)
+            {
+                if (!feof(fp))//文件结束：返回非0值；文件未结束：返回0值
+                {
+                    printf("fread error\n");
+                    return 0;
+                }
+                break;
+            }
+                p->data=tmp;
+                n++;
+              List_AddTail(list,p)
+        }
+        List_ForEach(list,q)
+        {
+            printf("%s\n",q->data.username);
+        }
+     
+    }
+    fclose(fp);
     return n;
 }
 int Account_Perst_SelByName(char usrName[],account_t * usr)
