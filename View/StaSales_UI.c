8ccd7ec2eeec35d75c2430ac1021413512ec33fe
ZZZcod <852304045@qq.com>
2021-06-17 20:18:39 +0800

    xxx

--- View/StaSales_UI.c
+++ View/StaSales_UI.c
@@ -11,6 +11,7 @@
 #include "stdio.h"
 #include "../Service/Play.h"
 #include "time.h"
+#include "../Service/StaSales.h"
 void StaSales_UI_MgtEntry(void)
 { 
     extern Gl_CurUser;
@@ -55,11 +56,11 @@ void StaSales_UI_Self(void)
     {
     case 'd':
     case 'D':
-        SalesAnalysis_Srv_CompSaleVal(id,curdate,curdate);
+        StaSales_Srv_CompSaleVal(id,curdate,curdate);
         break;
     case 'm':
     case 'M':
-        SalesAnalysis_Srv_CompSaleVal(id,startdate,enddate);
+        StaSales_Srv_CompSaleVal(id,startdate,enddate);
         break;
     }
     return ; 
@@ -78,7 +79,7 @@ void StaSales_UI_Clerk(void)
     }
     else
     {
-        SalesAnalysis_Srv_CompSaleVal(id,startdate,enddate);
+        StaSales_Srv_CompSaleVal(id,startdate,enddate);
     }
     return; 
 }
