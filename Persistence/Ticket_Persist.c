2831274f6346820589b625249f884a3ef6972541
ajian2002 <ajian020515@163.com>
2021-06-17 10:52:02 +0800

    aaa

--- Persistence/Ticket_Persist.c
+++ Persistence/Ticket_Persist.c
@@ -25,32 +25,6 @@ int Ticket_Perst_Update(const ticket_t* data)
     return rtn;
 }
 
-int Ticket_Srv_SelBySchID(int schedule_id, ticket_list_t list)
-{
-    List_Init(list, ticket_list_t)
-    ticket_t data;
-    int rtn  = 0;
-    FILE* fp = fopen("Ticket.dat", "r");
-    if (fp == NULL)
-    {
-        printf("file open fiaied\n");
-        return -1;
-    }
-    while (!feof(fp))
-    {
-        fread(&data, sizeof(ticket_t), 1, fp);
-        if (schedule_id == data.schedule_id)
-        {
-            ticket_list_t  tmp = (ticket_list_t)malloc(sizeof(ticket_list_t));
-            tmp->data         = data;
-            tmp->prev = tmp->next = tmp;
-            List_AddTail(list, tmp);
-            rtn += 1;
-        }
-    }
-    fclose(fp);
-    return rtn;
-}
 
 int Ticket_Perst_Insert(ticket_list_t list)
 {
