23773419cfb366216df21af7fe4a3a9f903a66a0
PENG <1337903705@qq.com>
2021-06-18 12:16:12 +0800

    Revert "sad"
    This reverts commit 2bfb4a456272bc537d966ca192bed850230e967e.

--- Service/Account.c
+++ Service/Account.c
@@ -4,11 +4,7 @@
 account_t Account_Srv_FetchbyUser(char usrName[])
 {
     account_t tmp;
-    if (Account_Perst_SelByName(usrName,&tmp)==0)
-    {
-        printf("未找到");
-    }
-    else return tmp;
+    return tmp;
 }
 int Account_Srv_Modify(account_t *data)
 {
