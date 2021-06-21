a79edd779b9c24b541c499b9e71c918828cad823
ajian2002 <ajian020515@163.com>
2021-06-21 14:39:43 +0800

    docs(asfa): a
    sf

--- Service/Ticket.c
+++ Service/Ticket.c
@@ -20,7 +20,7 @@ int Ticket_Srv_GenBatch(int schedule_id)
     }
     return number;
 }
-void Ticket_Srv_DeleteBatch(int schedule_id)
+int Ticket_Srv_DeleteBatch(int schedule_id)
 {
     int found = 0;
     found     = Ticket_Perst_Rem(schedule_id);
@@ -49,7 +49,7 @@ int Ticket_Srv_SelBySchID(int schedule_id, ticket_list_t list)
         fread(&data, sizeof(ticket_t), 1, fp);
         if (schedule_id == data.schedule_id)
         {
-            ticket_list_t tmp = (ticket_list_t)malloc(sizeof(ticket_list_t));
+            ticket_list_t tmp = (ticket_list_t)malloc(sizeof(ticket_node_t));
             tmp->data         = data;
             tmp->prev = tmp->next = tmp;
             List_AddTail(list, tmp);
@@ -68,32 +68,6 @@ ticket_node_t* Ticket_Srv_FetchBySeatID(ticket_list_t list, int seat_id)
         if (temp->data.seat_id == seat_id) return temp;
     }
     if (temp == list) return NULL;
-}
-int Ticket_Srv_SelBySchID(int schedule_id, ticket_list_t list)
-{
-    List_Init(list, *ticket_list_t);
-    ticket_t data;
-    int rtn  = 0;
-    FILE* fp = fopen("Ticket.dat", "r");
-    if (fp == NULL)
-    {
-        printf("file open fialed\n");
-        return -1;
-    }
-    while (!feof(fp))
-    {
-        fread(&data, sizeof(ticket_t), 1, fp);
-        if (schedule_id == data.schedule_id)
-        {
-            ticket_list_t tmp = (ticket_list_t)malloc(sizeof(ticket_list_t));
-            tmp->data         = data;
-            tmp->prev = tmp->next = tmp;
-            List_AddTail(list, tmp);
-            rtn += 1;
-        }
-    }
-    fclose(fp);
-    return rtn;
 }
 
 int Ticket_Srv_FetchBySchID(int schedule_id, ticket_list_t list)
