b1b4215b7288819dd60a03b516dada121ec5b6d0
raja <1647193241@qq.com>
2021-06-21 15:50:22 +0800

    a

--- Service/Account.c
+++ Service/Account.c
@@ -4,37 +4,39 @@ extern account_t gl_CurUser;
 account_t Account_Srv_FetchbyUser(char usrName[])
 {
     account_t tmp;
-    if (Account_Perst_SelByName(usrName,&tmp)==0)
+    if (Account_Perst_SelByName(usrName, &tmp) == 0)
     {
         printf("未找到");
     }
-    else return tmp;
+    else
+        return tmp;
 }
 int Account_Srv_Modify(account_t *data)
 {
     account_t tmp;
-    FILE*fp=fopen("Account.dat","rb+");
+    FILE *fp = fopen("Account.dat", "rb+");
     if (!fp)
     {
         printf("Account.dat fopen failed\n");
         return 0;
     }
-    else{
-        while(1)
+    else
+    {
+        while (1)
         {
-            if (fread(&tmp, sizeof(account_t), 1, fp)<1)
+            if (fread(&tmp, sizeof(account_t), 1, fp) < 1)
             {
-                if (!feof(fp))//文件结束：返回非0值；文件未结束：返回0值
+                if (!feof(fp))  //文件结束：返回非0值；文件未结束：返回0值
                 {
                     printf("fread error\n");
                     return 0;
                 }
                 break;
             }
-            if (tmp.username==data->username)
+            if (tmp.username == data->username)
             {
-                fseek(fp,-(int)sizeof(account_t),SEEK_CUR);
-                fwrite(data,sizeof(account_t), 1, fp);
+                fseek(fp, -(int)sizeof(account_t), SEEK_CUR);
+                fwrite(data, sizeof(account_t), 1, fp);
             }
         }
     }
@@ -44,18 +46,19 @@ int Account_Srv_Modify(account_t *data)
 int Account_Srv_Verify(char usrName[], char pwd[])
 {
     account_t usr;
-    if (Account_Perst_SelByName(usrName,&usr)==0)
+    if (Account_Perst_SelByName(usrName, &usr) == 0)
     {
         return 0;
     }
     else
     {
-        if (strcmp(pwd,usr.password)==0)
+        if (strcmp(pwd, usr.password) == 0)
         {
-            gl_CurUser=usr;
+            gl_CurUser = usr;
             return 1;
         }
-        else return 0;
+        else
+            return 0;
     }
 }
 void Account_Srv_InitSys()
@@ -66,18 +69,19 @@ void Account_Srv_InitSys()
     }
     else
     {
-        account_t admin={0,USR_ADMIN,"admin","12345"};
-        admin.id=EntKey_Perst_GetNewKeys("系统用户",1);
+        account_t admin = {0, USR_ADMIN, "admin", "12345"};
+        admin.id        = EntKey_Perst_GetNewKeys("系统用户", 1);
         Account_Srv_Add(&admin);
     }
 }
 int Account_Srv_Add(account_t *data)
 {
-    if (Account_Perst_Insert(data)<0)
+    if (Account_Perst_Insert(data) < 0)
     {
         return 0;
     }
-    else return 1;
+    else
+        return 1;
 }
 int Account_Srv_DeleteByID(int usrID)
 {
@@ -85,16 +89,17 @@ int Account_Srv_DeleteByID(int usrID)
     {
         return 1;
     }
-    else return 0;
+    else
+        return 0;
 }
 account_node_t *Account_Srv_FindByUsrName(account_list_t list, char usrName[])
 {
     account_node_t *q;
-    List_ForEach(list,q)
+    List_ForEach(list, q)
     {
-        if (strcmp(usrName,q->data.username)==0)
+        if (strcmp(usrName, q->data.username) == 0)
         {
-             return q;
+            return q;
         }
     }
     return NULL;
