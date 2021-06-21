1b6fe459cf2e7742fd1ca8166de4c3c7594e8b89
ZZZcod <852304045@qq.com>
2021-06-21 15:07:19 +0800

    sda

--- View/StaSales_UI.c
+++ View/StaSales_UI.c
@@ -8,11 +8,11 @@
  */
 #include "StaSales_UI.h"
 #include "../Common/Common.h"
-#include "../Service/Account.h"
 #include "../Service/Play.h"
 #include "../Service/StaSales.h"
 #include "stdio.h"
 #include "time.h"
+#include "../Persistence/Account_Persist.h"
 void StaSales_UI_MgtEntry(void)
 {
     extern account_t Gl_CurUser;
