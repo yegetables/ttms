debac92201a2728907ff1fb2223721700687f554
ZZZcod <852304045@qq.com>
2021-06-21 15:22:03 +0800

    mm

--- View/StaSales_UI.c
+++ View/StaSales_UI.c
@@ -81,7 +81,7 @@ void StaSales_UI_Clerk(void)
 {
     extern account_t Gl_CurUser;
     int id = Gl_CurUser.id;
-    ttms_date_t curdate, startdate, enddate;
+    ttms_date_t startdate, enddate;
     account_t tmp;
     char Usrname[1000];
     scanf("%s", Usrname);
