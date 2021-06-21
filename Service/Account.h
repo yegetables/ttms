633143d14a9572aeba58f2f127ec6284b7a1514e
ZZZcod <852304045@qq.com>
2021-06-21 15:03:13 +0800

    aa

--- Service/Account.h
+++ Service/Account.h
@@ -1,26 +1,6 @@
 #ifndef ACCOUNT_H_
 #define ACCOUNT_H_
 #include <stdio.h>
-typedef enum
-{
-    USR_ANOMY = 0,
-    USR_CLERK = 1,
-    USR_MANG  = 2,
-    USR_ADMIN = 9,
-} account_type_t;
-typedef struct
-{
-    int id;
-    account_type_t type;
-    char username[30];
-    char password[30];
-} account_t;
-typedef struct account_node
-{
-    account_t data;
-    struct account_node *next;
-    struct account_node *prev;
-} account_node_t, *account_list_t;
 void Account_Srv_InitSys();
 /**
  * @brief 验证系统用户的用户名和密码
