e15d5bbd9bcbda30951ece063aeb071787db08ea
A7SS5 <1337903705@qq.com>
2021-06-23 04:01:36 +0800

    asd

--- View/Main_Menu.c
+++ View/Main_Menu.c
@@ -9,6 +9,7 @@
 
 #include "Main_Menu.h"
 //系统主菜单
+extern account_t gl_CurUser;
 void Main_Menu(void)
 {
     if(SysLogin()==1)
@@ -22,14 +23,22 @@ void Main_Menu(void)
         printf(
             "**************** Theater Ticket Management System "
             "****************\n");
-        printf("[S]tudio Management.\n");
-        printf("[P]lay Management.\n");
-        printf("[T]icket Sale.\n");
-        printf("[R]eturn Ticket.\n");
+        if (gl_CurUser.type==9)
+        printf("[S]tudio Management.\n"); //管理演出厅
+        if (gl_CurUser.type==2)
+        printf("[P]lay Management.\n");  //管理剧目
+        if (gl_CurUser.type==1)
+        {
+        printf("[T]icket Sale.\n");  //售票
+        printf("[R]eturn Ticket.\n");  //退票
+        }
         // printf("[Q]ueries\n");
-        printf("Ra[N]king and Statistics.\n");
-        printf("[A]ccount Management.\n");
-        printf("[B]StaSales_UI_MgtEntry\n");
+        if (gl_CurUser.type==2)
+        printf("Ra[N]king and Statistics.\n"); //统计票房
+        if (gl_CurUser.type==9)
+        printf("[A]ccount Management.\n"); 
+        if (gl_CurUser.type==1||gl_CurUser.type==2)
+        printf("[B]StaSales_UI_MgtEntry\n");  //统计销售额
         printf("[E]xist.\n");
         printf(
             "\n================================================================"
@@ -41,10 +50,12 @@ void Main_Menu(void)
         {
             case 'S':
             case 's':
+            if (gl_CurUser.type==9)
                 Studio_UI_MgtEntry();
                 break;
             case 'P':
             case 'p':
+            if (gl_CurUser.type==2)
                 Play_UI_MgtEntry();
                 break;
             // case 'Q':
@@ -53,27 +64,32 @@ void Main_Menu(void)
             // break;
             case 'T':
             case 't':
+            if (gl_CurUser.type==1)
                 Sale_UI_MgtEntry();
                 break;
             case 'R':
             case 'r':
+            if (gl_CurUser.type==1)
                 Sale_UI_RetfundTicket();
                 break;
             case 'N':
             case 'n':
+            if (gl_CurUser.type==2)
                 SalesAnalysis_UI_MgtEntry();
                 break;
             case 'A':
             case 'a':
+                if (gl_CurUser.type==9)
                 Account_UI_MgtEntry();
                 break;
             case 'B':
             case 'b':
+            if (gl_CurUser.type==1||gl_CurUser.type==2)
                 StaSales_UI_MgtEntry();
                 break;
             case 'M':
             case 'm':
-                MainAccount_UI_MgtEntry();
+ //bug               MainAccount_UI_MgtEntry();
                 break;
         }
     } while ('E' != choice && 'e' != choice);
