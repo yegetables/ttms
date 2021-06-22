c5221f83eb66d02332a11eefa40b7546e82ba362
zhangang <1684505291@qq.com>
2021-06-22 15:31:41 +0800

    q

--- Persistence/Ticket_Persist.c
+++ Persistence/Ticket_Persist.c
@@ -1,10 +1,4 @@
 #include "Ticket_Persist.h"
-<<<<<<< HEAD
-#include "../Persistence/Play_Persist.h"
-#include "../Persistence/Schedule_Persist_Qry.h"
-#include "stdio.h"
-=======
->>>>>>> main
 int Ticket_Perst_Update(const ticket_t *data)
 {
     ticket_t ret;
