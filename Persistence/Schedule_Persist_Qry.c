14bdd4b71f1742628677a0ad6fcd577f772df31b
ajian2002 <ajian020515@163.com>
2021-06-23 01:11:35 +0800

    schedule fix

--- Persistence/Schedule_Persist_Qry.c
+++ Persistence/Schedule_Persist_Qry.c
@@ -1,7 +1,8 @@
-#include"Schedule_Persist_Qry.h"
+#include "Schedule_Persist_Qry.h"
 
 static const char SCHEDULE_DATA_FILE[] = "Schedule.dat";
-int Schedule_Perst_SelectAll(schedule_list_t list){
+int Schedule_Perst_SelectAll(schedule_list_t list)
+{
     schedule_node_t *newNode;
     schedule_t data;
     int recCount = 0;
@@ -16,7 +17,6 @@ int Schedule_Perst_SelectAll(schedule_list_t list){
         printf("文件打开失败%s\n", SCHEDULE_DATA_FILE);
         return 0;
     }
-
     while (!feof(fp))
     {
         if (fread(&data, sizeof(schedule_t), 1, fp))
@@ -34,17 +34,17 @@ int Schedule_Perst_SelectAll(schedule_list_t list){
             recCount++;
         }
     }
+
     fclose(fp);
     return recCount;
-
-
 }
 int Schedule_Perst_SelectByID(int id, schedule_t *buf)
 {
     int found = 0;
     FILE *fp  = fopen("Schedule.dat", "rb+");
-    if(fp == NULL){
-      printf("fail to open file");
+    if (fp == NULL)
+    {
+        printf("fail to open file\n");
     }
     if (feof(fp))
     {
@@ -54,7 +54,7 @@ int Schedule_Perst_SelectByID(int id, schedule_t *buf)
     else
     {
         schedule_t data;
-        while (feof(fp))
+        while (!feof(fp))
         {
             int x = fread(&data, sizeof(data), 1, fp);
             if (x == 0)
@@ -67,7 +67,7 @@ int Schedule_Perst_SelectByID(int id, schedule_t *buf)
                 if (data.id == id)
                 {
                     found = 1;
-                    buf   = &data;
+                    *buf  = data;
                 }
             }
         }
@@ -75,6 +75,3 @@ int Schedule_Perst_SelectByID(int id, schedule_t *buf)
         return found;
     }
 }
-
-
-
