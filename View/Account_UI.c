4fc38df220228628b5f2c2b168cbbb62d88edc66
A7SS5 <1337903705@qq.com>
2021-07-16 18:50:52 +0800

    as

--- View/Account_UI.c
+++ View/Account_UI.c
@@ -1,5 +1,23 @@
 #include "Account_UI.h"
-
+int getch()
+{
+   int c = 0;
+   struct termios org_opts, new_opts;
+   int res = 0;
+   //-----  store old settings -----------
+   res = tcgetattr(STDIN_FILENO, &org_opts);
+   assert(res == 0);
+   //---- set new terminal parms --------
+   memcpy(&new_opts, &org_opts, sizeof(new_opts));
+   new_opts.c_lflag &= ~(ICANON | ECHO | ECHOE | ECHOK | ECHONL | ECHOPRT | ECHOKE | ICRNL);
+   tcsetattr(STDIN_FILENO, TCSANOW, &new_opts);
+   c = getchar();
+   //------  restore old settings ---------
+   res = tcsetattr(STDIN_FILENO, TCSANOW, &org_opts);
+   assert(res == 0);
+   printf("*");
+   return c;
+}
 int SysLogin()  // SL界面
 {
     Account_Srv_InitSys();
@@ -37,11 +55,11 @@ int SysLogin()  // SL界面
     add_pass_1:
         printf("                              密码:");
         j = 0;
-        while ((n = getchar()) != '\n')
+        while ((n = getch()) != '\n')
         {
             if (n == ' ' || j >= 29)
             {
-                while ((n = getchar()) != '\n')
+                while ((n = getch()) != '\n')
                     ;
                 printf("密码不合规\n");
                 j = 0;
